c942b75a8c366f62aa39013dc1e28cca6b4b5f4a
Hayato Kohara <yaplus@users.noreply.github.com>
2019-10-05 16:56:21 +0900

    Initial Commit

---
+++ DJYusaku/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DJYusaku
+//
+//  Created by Hayato Kohara on 2019/10/05.
+//  Copyright © 2019 Yusaku. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DJYusaku/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DJYusaku
+//
+//  Created by Hayato Kohara on 2019/10/05.
+//  Copyright © 2019 Yusaku. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
