9ff700a988553ee83c82f3224ffa5a79963f4aa4
Yuu ichikawa <amylasef85@gmail.com>
2019-10-10 16:52:10 +0900

    指摘から - クラス名を訂正 - Info.plistにNSAppleMusicUsageDescriptionを追加 - メディアライブラリへのアクセス許可がない時に、設定画面から「メディアとAppleMusic」へのアクセス許可を行うためのアラートを追加

--- DJYusaku/FirstViewController.swift
+++ DJYusaku/FirstViewController.swift
@@ -9,34 +9,62 @@
 import UIKit
 import MediaPlayer
 
-class ViewController: UIViewController, MPMediaPickerControllerDelegate {
+class FirstViewController: UIViewController, MPMediaPickerControllerDelegate {
     
-    var player :MPMusicPlayerController!
+    var player = MPMusicPlayerController.applicationMusicPlayer
+    let picker = MPMediaPickerController()
+    let permissionStatus = MPMediaLibrary.authorizationStatus()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        player = MPMusicPlayerController.applicationMusicPlayer
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func displayPermissionAlert(){
+        let alert = UIAlertController(
+            title: "アクセス許可の要求",
+            message: "設定->DJ-YUSAKUを開いて、「メディアとAppleMusic」へのアクセス許可をしてください",
+            preferredStyle: .alert
+        )
+        alert.popoverPresentationController?.sourceView = self.view
+        
+        alert.addAction(UIAlertAction(title: "許可しない", style: .default, handler: nil))
+        
+        
+        let openSettingsAction = UIAlertAction(title: "「設定」を開く", style: .default){ _ in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alert.addAction(openSettingsAction)
+        
+        self.presentedViewController?.dismiss(animated: false, completion: nil)
+        present(alert, animated: true, completion: nil)
+        
+    }
 
     @IBAction func jumpToLibraryButton(_ sender: Any) {
-        let picker = MPMediaPickerController()
-        picker.delegate = self
-        picker.allowsPickingMultipleItems = true
-        present(picker, animated: true, completion: nil)
+        if(permissionStatus == .denied || permissionStatus == .restricted){
+            displayPermissionAlert()
+        }else{
+            picker.delegate = self
+            picker.allowsPickingMultipleItems = true
+            present(picker, animated: true, completion: nil)
+        }
     }
+    
     func mediaPicker(_ mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
         player.setQueue(with: mediaItemCollection)
         player.play()
         dismiss(animated: true, completion: nil)
     }
+    
     func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
-        
         dismiss(animated: true, completion: nil)
     }
     @IBAction func playButton(_ sender: Any) {
