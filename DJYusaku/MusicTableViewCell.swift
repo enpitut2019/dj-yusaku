d9180a9c27163fd8239ab53599e50e5650287d76
Hayato Kohara <yaplus@users.noreply.github.com>
2019-10-12 11:22:52 +0900

    検索画面のTableViewにSeachViewControllerをdataSourceに設定

---
+++ DJYusaku/MusicTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MusicTableViewCell.swift
+//  DJYusaku
+//
+//  Created by Hayato Kohara on 2019/10/12.
+//  Copyright © 2019 Yusaku. All rights reserved.
+//
+
+import UIKit
+
+class MusicTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var artist: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- DJYusaku/SearchViewController.swift
+++ DJYusaku/SearchViewController.swift
@@ -1,18 +1,25 @@
 //
-//  SecondViewController.swift
+//  SearchMusicTableViewController.swift
 //  DJYusaku
 //
-//  Created by Hayato Kohara on 2019/10/05.
+//  Created by Hayato Kohara on 2019/10/12.
 //  Copyright © 2019 Yusaku. All rights reserved.
 //
 
 import UIKit
 import StoreKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let results = [
+        MusicDataModel(title: "Maybe I'm Amazed", artist: "Paul McCartney"),
+        MusicDataModel(title: "Sir Duke", artist: "Stevie Wonder"),
+        MusicDataModel(title: "Rock With You", artist: "Michael Jackson")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         SKCloudServiceController.requestAuthorization { status in
             guard status == .authorized else { return }
@@ -20,6 +27,23 @@ class SearchViewController: UIViewController {
         }
     }
 
+    // MARK: - UITableViewDataSource
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MusicTableViewCell", for: indexPath) as! MusicTableViewCell
+        
+        let item = results[indexPath.row]
+        cell.title.text = item.title
+        cell.artist.text = item.artist
+
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
 
 }
-
