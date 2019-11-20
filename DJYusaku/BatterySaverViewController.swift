0bd0d49f4fe110927f1b4ca11d994fb3c6a4f711
Hayato Kohara <yaplus@users.noreply.github.com>
2019-11-20 17:45:07 +0900

    アプリがアクティブ・非アクティブになっても画面の明るさが元に戻るようにした

--- DJYusaku/BatterySaverViewController.swift
+++ DJYusaku/BatterySaverViewController.swift
@@ -17,9 +17,12 @@ class BatterySaverViewController: UIViewController {
         
         // ダブルタップジェスチャを追加
         let doubleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
-                                                                              action:#selector(handleDoubleTapeed(_:)))
+                                                                              action: #selector(handleDoubleTapeed(_:)))
         doubleTapGesture.numberOfTapsRequired = 2
         self.view.addGestureRecognizer(doubleTapGesture)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleWillResignActiveNotification), name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleDidBecomeActiveNotification), name: UIApplication.didBecomeActiveNotification, object: nil)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -55,6 +58,22 @@ class BatterySaverViewController: UIViewController {
     // 画面のどこかしらがダブルタップされたら
     @objc func handleDoubleTapeed(_ gesture: UITapGestureRecognizer) -> Void {
         self.dismiss(animated: true)  // Viewを閉じる
+    }
+    
+    // アプリがアクティブになる（例：アプリが他のタスクから復帰する）とき
+    @objc func handleDidBecomeActiveNotification() {
+        // 自動スリープをOFFにする
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // 画面の明るさを最低にする
+        self.previousScreenBrightness = UIScreen.main.brightness
+        UIScreen.main.brightness = 0.0
+    }
+    
+    // アプリがアクティブじゃなくなる（例：ホーム画面に戻る）とき
+    @objc func handleWillResignActiveNotification() {
+        UIApplication.shared.isIdleTimerDisabled = false    // 自動スリープをONにする
+        UIScreen.main.brightness = previousScreenBrightness // 画面の明るさを復元する
     }
     
     // ホームインジケータ(iPhone X以降)を非表示にする
