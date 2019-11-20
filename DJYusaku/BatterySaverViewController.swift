d7d7abef5ca1e00feae78ac920b7a83a8f7f8f1d
Hayato Kohara <yaplus@users.noreply.github.com>
2019-11-20 19:02:05 +0900

    ジャムバタチーズだんだんだん

--- DJYusaku/BatterySaverViewController.swift
+++ DJYusaku/BatterySaverViewController.swift
@@ -29,7 +29,7 @@ class BatterySaverViewController: UIViewController {
         super.viewDidAppear(animated)
         
         // アラートを表示
-        let alertController = UIAlertController(title:   "Batter Saver Mode",
+        let alertController = UIAlertController(title:   "Battery Saver Mode",
                                                 message: "To exit battery saver mode, double-tap the screen.",
                                                 preferredStyle: UIAlertController.Style.alert)
         let alertButton = UIAlertAction(title: "OK",
