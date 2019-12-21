a06d9efdaef0a4da8eb219aba75eb4ac3627b068
Hayato Kohara <yaplus@users.noreply.github.com>
2019-12-21 17:51:08 +0900

    UserDefaultsでログイン状態を保存するようにした

--- DJYusaku/SettingViewController.swift
+++ DJYusaku/SettingViewController.swift
@@ -11,37 +11,54 @@ import Swifter
 import SafariServices
 
 class SettingViewController: UIViewController, SFSafariViewControllerDelegate {
-    private var swifter = Swifter(
-        consumerKey: Secrets.TwitterConsumerKey,
-        consumerSecret: Secrets.TwitterConsumerSecret
-    )
-    
-    private var screenName: String?
+    private var swifter : Swifter = Swifter(consumerKey: Secrets.TwitterConsumerKey,
+                                            consumerSecret: Secrets.TwitterConsumerSecret)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // ログイン状態が保存されていればプロフィールを取得する
+        if let twitterKey        = UserDefaults.standard.string(forKey: UserDefaults.DJYusakuDefaults.TwitterKey)
+         , let twitterSecret     = UserDefaults.standard.string(forKey: UserDefaults.DJYusakuDefaults.TwitterSecret)
+         , let twitterScreenName = UserDefaults.standard.string(forKey: UserDefaults.DJYusakuDefaults.TwitterScreenName) {
+            swifter = Swifter(consumerKey: Secrets.TwitterConsumerKey,
+                              consumerSecret: Secrets.TwitterConsumerSecret,
+                              oauthToken: twitterKey,
+                              oauthTokenSecret: twitterSecret)
+            // プロフィールを取得する
+            print(twitterScreenName)
+            self.getProfile(screenName: twitterScreenName)
+        }
     }
     
     @IBAction func loginTwitter(_ sender: Any) {
         let url = URL(string: "swifter://success")!
         swifter.authorize(withCallback: url, presentingFrom: self, success: { [unowned self] token, _ in
-            if token != nil {
-                self.screenName = token!.screenName
-            }
-            // ユーザーのプロフィール情報を取得して設定する
-            self.swifter.showUser(.screenName(self.screenName!), success: { json in
-                let imageUrlString = json["profile_image_url_https"].string!.replacingOccurrences(of: "_normal", with: "", options: .backwards)
-                let profile = PeerProfile(name:     json["name"].string!,
-                                          imageUrl: URL(string: imageUrlString)!)
-                ConnectionController.shared.setProfile(profile: profile)
-                NotificationCenter.default.post(name: .DJYusakuPeerConnectionStateDidUpdate, object: nil)
-                // プロフィールを他のピアに送信する
-                let data = try! JSONEncoder().encode(profile)
-                let messageData = try! JSONEncoder().encode(MessageData(desc:  MessageData.DataType.peerProfile, value: data))
-                ConnectionController.shared.session.sendRequest(messageData, toPeers: ConnectionController.shared.session.connectedPeers, with: .unreliable)
-            }, failure: { error in
-                self.alert(title: "Error", message: error.localizedDescription)
-            })
+            guard token != nil else { return }
+            // ログイン状態を保存する
+            UserDefaults.standard.set(token?.key,         forKey: UserDefaults.DJYusakuDefaults.TwitterKey)
+            UserDefaults.standard.set(token?.secret,      forKey: UserDefaults.DJYusakuDefaults.TwitterSecret)
+            UserDefaults.standard.set(token!.screenName!, forKey: UserDefaults.DJYusakuDefaults.TwitterScreenName)
+
+            self.getProfile(screenName: token!.screenName!)
+        }, failure: { error in
+            self.alert(title: "Error", message: error.localizedDescription)
+        })
+    }
+    
+    func getProfile(screenName: String) {
+        
+        // ユーザーのプロフィール情報を取得して設定する
+        self.swifter.showUser(.screenName(screenName), success: { json in
+            let imageUrlString = json["profile_image_url_https"].string!.replacingOccurrences(of: "_normal", with: "", options: .backwards)
+            let profile = PeerProfile(name:     json["name"].string!,
+                                      imageUrl: URL(string: imageUrlString)!)
+            ConnectionController.shared.setProfile(profile: profile)
+            NotificationCenter.default.post(name: .DJYusakuPeerConnectionStateDidUpdate, object: nil)
+            // プロフィールを他のピアに送信する
+            let data = try! JSONEncoder().encode(profile)
+            let messageData = try! JSONEncoder().encode(MessageData(desc:  MessageData.DataType.peerProfile, value: data))
+            ConnectionController.shared.session.sendRequest(messageData, toPeers: ConnectionController.shared.session.connectedPeers, with: .unreliable)
         }, failure: { error in
             self.alert(title: "Error", message: error.localizedDescription)
         })
