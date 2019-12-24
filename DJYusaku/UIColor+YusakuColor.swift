d67bd9947d7f5b68a066a4787f5f2440ee130cce
Hayato Kohara <yaplus@users.noreply.github.com>
2019-12-25 00:29:17 +0900

    アセットに優作ピンクを追加

---
+++ DJYusaku/UIColor+YusakuColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+YusakuColor.swift
+//  DJYusaku
+//
+//  Created by Hayato Kohara on 2019/12/24.
+//  Copyright © 2019 Yusaku. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let yusakuPink = UIColor(named: "YusakuPinkColor")
+}
