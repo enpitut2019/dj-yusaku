95e8a215944401f39e308b88c78d79f42c726420
Yuu ichikawa <amylasef85@gmail.com>
2020-02-19 14:31:32 +0900

    BatterySaverViewでのNowPlaying表示を再度実装 既存のnoteViewに必要なパーツを追加する形に変更し再実装した 必要な処理は前回のものと同様に記述 オプションで設定するところも前回と同じ

--- DJYusaku/BatterySaverViewController.swift
+++ DJYusaku/BatterySaverViewController.swift
@@ -11,11 +11,15 @@ import UIKit
 class BatterySaverViewController: UIViewController {
     
     @IBOutlet weak var noteView: UIView!
+    @IBOutlet weak var nowPlayingArtwork: UIImageView!
+    @IBOutlet weak var nowPlayingTitle: UILabel!
+    @IBOutlet weak var nowPlayingArtist: UILabel!
+    
     private var previousScreenBrightness : CGFloat = 0.0    // 元の画面の明るさ
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         // シングルタップジェスチャを追加
         let singleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
                                                                               action: #selector(handleSingleTapeed(_:)))
@@ -33,6 +37,7 @@ class BatterySaverViewController: UIViewController {
         
         NotificationCenter.default.addObserver(self, selector: #selector(handleWillResignActiveNotification), name: UIApplication.willResignActiveNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(handleDidBecomeActiveNotification), name: UIApplication.didBecomeActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleNowPlayingItemDidChange), name: .DJYusakuPlayerQueueNowPlayingSongDidChange, object: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,8 +49,19 @@ class BatterySaverViewController: UIViewController {
         // 元の画面の明るさを記録しておく
         self.previousScreenBrightness = UIScreen.main.brightness
         
+        if(PlayerQueue.shared.isQueueCreated && DefaultsController.shared.isNowPlayingDisplayEnabled){
+            updateNowPlaying()
+            nowPlayingArtwork.alpha = 1.0
+            nowPlayingTitle.alpha   = 1.0
+            nowPlayingArtist.alpha  = 1.0
+        }else{
+            nowPlayingArtwork.alpha = 0.0
+            nowPlayingTitle.alpha   = 0.0
+            nowPlayingArtist.alpha  = 0.0
+        }
+        
         // 注意書きを表示してフェードアウトする
-        self.animateFadeOut(view: self.noteView)
+        self.animateFadeOut(view: self.noteView, delay: 1.0)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -63,10 +79,10 @@ class BatterySaverViewController: UIViewController {
         NotificationCenter.default.post(name: .DJYusakuModalViewDidDisappear, object: nil)
     }
     
-    func animateFadeOut(view: UIView) {
+    func animateFadeOut(view: UIView, delay: Double) {
         view.alpha = 1.0
         UIScreen.main.brightness = previousScreenBrightness
-        UIView.animate(withDuration: 2.0, delay: 1.0, animations: {
+        UIView.animate(withDuration: 2.0, delay: delay, animations: {
             view.alpha = 0.0
         }, completion: { finished in
             if finished {
@@ -76,10 +92,26 @@ class BatterySaverViewController: UIViewController {
         })
     }
     
+    private func updateNowPlaying(){
+        let indexNowPlayingItem = PlayerQueue.shared.mpAppController.indexOfNowPlayingItem;
+        if let nowPlayingSong = PlayerQueue.shared.get(at: indexNowPlayingItem) {
+            DispatchQueue.global().async {
+                let image = CachedImage.fetch(url: nowPlayingSong.artworkUrl)
+                DispatchQueue.main.async {
+                    self.nowPlayingArtwork.image = image
+                    self.nowPlayingTitle.text  = nowPlayingSong.title
+                    self.nowPlayingArtist.text = nowPlayingSong.artist
+                }
+            }
+        }
+    }
+    
+    
+    
     // 画面のどこかしらがシングルタップされたら
     @objc func handleSingleTapeed(_ gesture: UITapGestureRecognizer) -> Void {
         // 注意書きを表示してフェードアウトする
-        self.animateFadeOut(view: self.noteView)
+        self.animateFadeOut(view: self.noteView, delay: 1.0)
     }
 
     // 画面のどこかしらがダブルタップされたら
@@ -96,7 +128,16 @@ class BatterySaverViewController: UIViewController {
         self.previousScreenBrightness = UIScreen.main.brightness
         
         // 注意書きを表示してフェードアウトする
-        self.animateFadeOut(view: self.noteView)
+        self.animateFadeOut(view: self.noteView, delay: 1.0)
+    }
+    
+    //NowPlayingの内容が変わったら更新して再表示
+    @objc func handleNowPlayingItemDidChange(){
+        if(DefaultsController.shared.isNowPlayingDisplayEnabled){
+            self.noteView.layer.removeAllAnimations()
+            updateNowPlaying()
+            self.animateFadeOut(view: self.noteView, delay: 5.0)
+        }
     }
     
     // アプリがアクティブじゃなくなる（例：ホーム画面に戻る）とき
